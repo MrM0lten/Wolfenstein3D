ce31779e1d64d8bf6886e580cbedc61d8411db70
MrM0lten <jisserst@c4c6c6.42berlin.de>
2023-04-03 18:42:15 +0200

    added player cube representation

--- src/main.c
+++ src/main.c
@@ -10,21 +10,27 @@
 #define IMG_HEIGHT 512
 #define SQ_DIM 64
 
-/* void ft_hook(void* param)
+int p_x = 256;
+int p_y = 256;
+
+mlx_image_t* image;
+
+void ft_hook(void* param)
 {
 	mlx_t* mlx = param;
 
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
+		p_y -= 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
+		p_y += 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
+		p_x -= 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-} */
+		p_x += 1;
+}
+
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
@@ -34,21 +40,23 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 //draws a rectangle to an imagine based on some dimensions
 void draw_rect(mlx_image_t* image, int x, int y, int val)
 {
-	if(val != 1)
-		return ;
 	//calculate max position -1 to create a border around squares
 	int x_max = x*SQ_DIM + SQ_DIM -1;
 	int y_max = y*SQ_DIM + SQ_DIM -1;
 	for (int i = y*SQ_DIM; i < y_max; i++)
 	{
 		for (int j = x*SQ_DIM; j < x_max; j++)
+		{
+			if(val == 1)
 				mlx_put_pixel(image, j, i, 0xFFFFFFFF);
+			else
+				mlx_put_pixel(image, j, i, 0x000000FF);
+		}
 
 	}
 }
 
-
-int main()
+void draw_minimap(void* param)
 {
 	int x = 8;
 	int y = 8;
@@ -63,18 +71,38 @@ int main()
 	1,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1,
 };
-	printf("Hello\n");
-	mlx_t *mlx = mlx_init(IMG_WIDTH, IMG_HEIGHT, "wolfenstein", true);
-	if (!mlx)
-		printf("error\n");
-	mlx_image_t* image = mlx_new_image(mlx, IMG_WIDTH, IMG_HEIGHT);
 
 	for (int i = 0; i < y; i++)
 	{
 		for (int j = 0; j < x; j++)
 			draw_rect(image, j, i, arr[(i*8)+j]);
 	}
+
+	//draw player
+	int p_size = 5;
+	for (int i = 0; i < p_size; i++)
+	{
+		for (int j = 0; j < p_size; j++)
+			mlx_put_pixel(image, p_x+j, p_y+i, 0xFF0000FF);
+	}
+}
+
+int main()
+{
+
+	mlx_t* mlx = mlx_init(IMG_WIDTH, IMG_HEIGHT, "wolfenstein", true);
+	if (!mlx)
+		printf("error\n");
+
+
+	image = mlx_new_image(mlx, IMG_WIDTH / 2, IMG_HEIGHT);
+
+
+	mlx_loop_hook(mlx, draw_minimap, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_image_to_window(mlx, image, 0, 0);
 
 	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (0);
 }
